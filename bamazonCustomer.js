67bbaef68ded3a683713bf013ed3b97fb8243b3a
Evan Darst <evan.l.darst@gmail.com>
2018-01-15 21:38:10 -0500

    created database connection and tested to return unique connection ID

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,19 @@
 var sql = require("mysql");
 var inquirer = require("Inquirer");
+
+var thisDepartment;
+var updateSaleCount;
+var amount;
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: '@@Widget1188',
+	database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+  });
