ad774ba27cd5def76b5a17a1d118dcd860efbcfb
Evan Darst <evan.l.darst@gmail.com>
2018-01-15 21:47:21 -0500

    function created to loop through database and show products for customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,7 @@
-var sql = require("mysql");
-var inquirer = require("Inquirer");
+//Installed and required npm packages
 
-var thisDepartment;
-var updateSaleCount;
-var amount;
+var mysql = require("mysql");
+var inquirer = require("Inquirer");
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -13,7 +11,27 @@ var connection = mysql.createConnection({
 	database: "bamazon"
 });
 
+//Create connection to database
+
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
+    showProducts();
   });
+
+  //Display items in store
+
+  function showProducts() {
+
+    connection.query("SELECT * FROM products", function(err, res){
+		if (err) throw err;
+		console.log("-------------------------------------------------");
+		console.log("----Select from the following products-----------");
+		console.log("-------------------------------------------------");
+
+		for (i=0; i < res.length; i++) {
+			console.log("Item number: " + res[i].id + "Product Name: " + res[i].ProductName + " Price: " + "$" + res[i].Price + "(Quantity left: " + res[i].StockQuantity + ")")
+		}
+		console.log("-------------------------------------------------");
+		})
+}
